e85393b06802e9fcf867b04fe6b2f8012250ca1c
Mercen19 <61963526+Mercen19@users.noreply.github.com>
2021-03-03 12:52:44 +0700

    store message components

---
+++ script.js
@@ -0,0 +1,5 @@
+const message = {
+    signInfo: ['star', 'moon', 'sun', 'comet'],
+    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
+}
